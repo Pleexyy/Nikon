eebfaed3e48f8527343800228a4c853d627f5ec5
Thibaud <thibaud.bonneville@gmail.com>
2020-09-02 09:10:51 +0200

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -1,5 +1,5 @@
 <?php
-   // mis en place d'un option lors de la connexion à la bdd pour prendre en compte les accents et autres caractères
+   // mis en place d'une option lors de la connexion à la bdd pour prendre en compte les accents et autres caractères
    $options = array(
       PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
    );
